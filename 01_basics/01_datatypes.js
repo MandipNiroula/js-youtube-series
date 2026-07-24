d93165d2131e5268dc48d92c19089a8e16bbf635
MandipNiroula <mandipneroula@gmail.com>
2026-07-24 04:24:09 +0000

    datatypes

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; //treat all js code as newer version
+
+// alert ( 3 + 3 ) | we are using nodejs, not browser
+
+// code should be readable
+
+ console.log( 9
+    + 7
+)
+// in the above example the code is not readable 
+
+let name = "mandip"
+let age = 20
+let isLoggedIn = true
+
+// number => 2 t0 power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => value is not defined
+// symbol => unique
+
+// object
+
+console.log(typeof "mandip") // output : string
+console.log(typeof undefined) // output : undefined
+console.log(typeof null) // output : object
+ 
